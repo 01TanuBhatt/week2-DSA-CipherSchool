69e44f327aa5a15520ee6d6165ed80186cc56522
Tanu Bhatt <01tanubhatt@gmail.com>
2023-07-02 17:37:32 +0530

    eighth commit

---
+++ week2-lecture8.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class TreeNode{
+    public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr),right(nullptr) {}
+    TreeNode(int x): val(x),left(nullptr),right(nullptr) {}
+    TreeNode(int x,TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+void inorder(TreeNode *root,vector<int> &result){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    result.push_back(root->val);
+    inorder(root->right);
+}
+vector<int> inorder(TreeNode *root){
+      vector<int> result;
+      inorder(root,result);
+      return result;
+}
+vector<int> presorder(TreeNode* root){
+    if(root == NULL){
+        return{};
+    }
+    vector<int> result;
+    stack<TreeNode *>S;
+    S.push(root);
+    TreeNode *temp;
+    while(true) {
+
+        while(temp != NULL && temp->left !=NULL){
+            result.push_back(temp->val);
+            temp=temp->left;
+        }
+        if(!S.empty()) {
+          break;  
+        }
+        temp =S.top();S.pop();
+             temp=temp->right;
+       
+    }
+    return result;
+}
+
+void printGivenLevel(TreeNode* root,int level,vector<int> &levelH){
+    if(root == NULL)
+    {
+        return;
+    }
+    if(level == 0){
+        levelH.push_back(root->val);
+        } else if(level>0){
+            printGivenLevel(root->left,level-1,levelH);
+            printGivenLevel(root->right,level-1,levelH);
+
+        }
+}
+int height(TreeNode* root) {
+    if(root == NULL){
+        return 0;
+    }
+    else{
+        return 1+max(height(root->left),height(root->right));
+    }
+}
+vector<vector<int>> levelOrder(TreeNode* root){
+    if(root == NULL){
+        return {};
+    }
+    vector<vector<int>> levelOrder;
+    int H = height(root);
+    for(int h=1;h<=H;h++) {
+        vector<int> levelH;
+        printGivenLevel(root,h,levelH);
+        levelOrder.push_back(levelH);
+    }
+    return levelOrder;
+}
+
+vector<vector<int>> levelOrderLineByLineIterative(TreeNode* root) {
+    if(root ==NULL) {
+        return {};
+    }
+    vector<vector<int>> result;
+    vector<int> currLevel;
+    queue<TreeNode*> Q;
+    Q.push(root);
+    Q.push(NULL);
+    while(!Q.empty()) {
+        root =Q.front();Q.pop();
+        if(root == NULL) {
+            result.push_back(currLevel);
+            currLevel.clear();
+            if(!Q.empty()) {
+                    Q.push(NULL);
+            }
+        }else{
+        currLevel.push_back(root->val);
+        if(root->left != NULL)
+        Q.push(root->left);
+        if(root->right != NULL)
+        Q.push(root->right);
+        }
+       
+    }
+    return result;
+}
+
+// int diameter(TreeNode* root){
+//     if(root == NULL) {
+//         return 0;
+//     }
+//     int lh = height(root->left);
+//     int rh = height(root->right);
+//     return max(lh+rh+1,max(diameter(root->left),diameter(root->right)));
+// }
+
+int diameter(TreeNode *root,int *height){
+    if(root == NULL){
+        *height =0;
+        return 0;
+    }
+    int lh=0,rh=0,ld=0,rd=0;
+    ld=diameter(root->left,&lh);
+    rd=diameter(root->right,&rh);
+
+    *height = max(lh,rh)+1;
+    return max(lh+rh+1,max(ld,rd));
+}
+int main(){
+
+}
